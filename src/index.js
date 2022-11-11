ce281ce2e7b90c8e5c846125ad73e57a24c5ac51
jbarbadillo <jbarbadillo@das-nano.com>
2022-11-11 08:45:39 +0100

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import '../node_modules/highlight.js/styles/ocean.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Docs />
-  </React.StrictMode>
-);
+ReactDOM.render(<Docs />, document.getElementById('root'));
+registerServiceWorker();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
